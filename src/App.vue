4ca05dceb2a7ec755535306aab0430dd3778edad
Diego Pérez <dperez2055@gmail.com>
2024-07-17 03:05:35 -0300

    Refactor App.vue to use script setup and remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
+<script setup>
+import Blog from "./components/Blog"
+const title = "Blog";
+</script>
 <template>
   <div>
     <h8k-navbar v-bind:header="title"></h8k-navbar>
     <Blog/>
   </div>
 </template>
-
-<script>
-import Blog from "./components/Blog";
-export default {
-  data() {
-    return {
-      title : "Blog",
-    }
-  },
-  components : {
-    Blog
-  },
-};
-
-</script>
 <style lang="scss">
 @import '../node_modules/h8k-design/dist/index.css';
 </style>
